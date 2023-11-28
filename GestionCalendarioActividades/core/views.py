02bfc696335e17865ad33a93c18c578db3bb4ed5
JeremySantander <jeremy.santander@usm.cl>
2023-11-28 04:33:31 -0300

    Añadido el filtro a el view

--- GestionCalendarioActividades/core/views.py
+++ GestionCalendarioActividades/core/views.py
@@ -4,8 +4,13 @@ from .models import Evento
 # Create your views here.
 def index(request):
     EventoS = Evento.objects.all().order_by('-fecha_inicio')
+    filtroSegmento = request.POST.get('FiltrarSegmento')
+    if filtroSegmento:
+        EventoS = EventoS.filter(segmento=filtroSegmento)
+    filtroTipo = request.POST.get('FiltrarTipo')
+    if filtroTipo:
+        EventoS = EventoS.filter(tipo=filtroTipo)
     datos = {
-        "Comunicados":Evento,
-        
+        "Evento":Evento,
         }
     return render(request, 'core/base.html', datos)
